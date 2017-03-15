923fd5a00d63b8516b48236911e691e04a63de97
haaningmkl <haaningmkl2@outlook.dk>
2017-03-15 13:39:02 +0100

    Add files via upload
    Galleri med slideshow. Anvender en ny style.css kaldet style2.css. for at få headeren til at virke.

--- Galleri.php
+++ Galleri.php
@@ -1,12 +1,6 @@
+
 <?php include("header.php");?>
-  
-<div id="test">
-  <div id="slideShowImages">
-    <img src="billeder/img_fjords.jpg" alt="Slide 1" />
-    <img src="billeder/img_forest.jpg" alt="Slide 2" />
-  </div>  
-  <button id="slideShowButton"></button> <!-- Optional button element. -->
- </div>
-  <script src="slideShow.js"></script>
+
+<?php include("index2.php");?>
 
 <?php include("footer.php");?>
