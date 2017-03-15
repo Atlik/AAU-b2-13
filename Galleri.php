43268d83bac5cdc40639fd77ceb9d0bb8e051b6c
haaningmkl <haaningmkl2@outlook.dk>
2017-03-15 11:51:03 +0100

    Start filer
    Der er en header og en footer som er tilføjet til index.php via include. 
    header.php indeholder navigationsmenuen.

---
+++ Galleri.php
@@ -0,0 +1,12 @@
+<?php include("header.php");?>
+  
+<div id="test">
+  <div id="slideShowImages">
+    <img src="billeder/img_fjords.jpg" alt="Slide 1" />
+    <img src="billeder/img_forest.jpg" alt="Slide 2" />
+  </div>  
+  <button id="slideShowButton"></button> <!-- Optional button element. -->
+ </div>
+  <script src="slideShow.js"></script>
+
+<?php include("footer.php");?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer style="position: fixed; bottom: 0px; width: 100%; height: 3%; background-color: #408E2F">
+    Copyright information
+  </footer>
+</body>
+</html>
