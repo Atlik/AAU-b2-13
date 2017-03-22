c1a75cc945805032f0f743a5a76bf99da4a21fe6
haaningmkl <haaningmkl2@outlook.dk>
2017-03-22 12:29:46 +0100

    Add files via upload

---
+++ FlatDesign/galleri_indhold.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Javascript slideshow</title>
+  
+  
+  
+      <link rel="stylesheet" href="style2.css">
+
+  
+</head>
+
+<body>
+
+    <div class="wrapper">
+    <h1>Se billeder fra tidligere events her.</h1>
+    <div class="slides">
+      <div class="slidesContainer">
+        <div class="slide"><img src="img_1.jpg" alt="billede"></div>
+        <div class="slide"><img src="img_2.jpg" alt="billede"></div>
+        <div class="slide"><img src="img_3.jpg" alt="billede"></div>
+        <div class="slide"><img src="img_4.jpg" alt="billede"></div>
+        <div class="slide"><img src="img_5.jpg" alt="billede"></div>
+      </div>
+    </div>
+    <ul class="buttons"></ul>
+    <p>Se mange flere billeder i vores galleri <a href="#">her.</a>
+    
+  </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+      <script src="index.js"></script>
+
+</body>
+</html>
