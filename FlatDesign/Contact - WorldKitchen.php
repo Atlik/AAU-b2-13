934a95a3b77f1665482e7bef78e16c924b3b799a
Christian <cholmg15@student.aau.dk>
2017-04-24 11:40:23 +0200

    moved items

---
+++ FlatDesign/Contact - WorldKitchen.php
@@ -0,0 +1,81 @@
+<html>
+    <meta charset="utf-8">
+<link rel="stylesheet"type="text/css"href="Contact - CSS.css">
+
+    <head>
+        <title>Contact us</title>
+    </head>
+    
+<body>
+    <div id="pageFont">
+<p id="HomeP">Home</p> &nbsp
+<p id="Triangle1P">&#9658</p>
+<p id="ContactP">Contact</p> 
+<p id="Trinagle2P">&#9658</p> 
+<p id="ContactP">Contact us</p>
+ 
+    <br> <br>
+    <h1>Contact us</h1>
+    
+    
+    <div class="ContactInfo">
+ 
+        <h2>Worldkitchen</h2>
+        <p> Catarina Figuerdo Bettencourt        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp       +45 12 34 56 78</p>
+        <p> 3th Floor, Aalborg University       &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp      CatarinaBettencourt@gmail.com</p>
+        <div class="locationpin"> <img src="locationpin.png" height="35" width="23"> </div>
+        <p> Denmark, Aalborg 9000</p>
+
+    </div>
+</div>
+   
+ <form action="###" method="POST">
+   
+    <div class="InformationFields">
+            
+        <input type="text" id="nameField" name="name" placeholder= "Your Name:" required="required"> <br><br>
+        <input type="text" id="topicField" name="topic" placeholder= "Your Topic:" required="required"> <br><br>
+        <input type="text" id="mailField" name="mail" placeholder= "Your Mail:" required="required"> <br><br>
+        <input type="text" id="phonenumberField" name="phonenumber" placeholder= "Your Phonenumber:" required="required"> <br><br>
+        <input type="text" id="messageField" name="message" placeholder="Your Message:" required="required" 
+    </div>    
+
+   <div>
+        <input type="submit" id="submitField" value="Submit"> 
+    </div>
+   
+ </form>
+  <div id="pageFont">  
+   <div class="Footer1">
+    
+    <h3>Menu &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Contact</h3>
+    <!--<h4>Contact</h4> -->
+      <div class="footerBoxes">
+        <ul type="square">
+   <li>Home</li> 
+   <li>Blog</li> 
+   <li>About</li> 
+   <li>Contact</li> 
+   <li>Buy ticket</li>
+         </ul>
+      </div>
+      
+    <div class=footerContactInfo>
+      Name (Catarina) <br>
+      Address (Gammeltorv) <br>
+      Region (Aalborg) <br>
+      Country (Denmark) <br> <br>
+      Email@email.com <br>
+      +45 69696969
+    </div>
+
+ 
+   <div class="Footer2">
+    Copyright © 2017 Worldconflict. All rights reserved.
+   </div>
+   </div> 
+</body>
+<?php
+
+?>
+</html>
