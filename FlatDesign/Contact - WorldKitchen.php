2960e7882b464700092a6548b13dca39d0b08629
LenovoZ500-pc\Nikolaj <nikolaj730@hotmail.com>
2017-04-24 12:00:18 +0200

    Mellemrum paragraph

--- FlatDesign/Contact - WorldKitchen.php
+++ FlatDesign/Contact - WorldKitchen.php
@@ -9,9 +9,13 @@
 <body>
     <div id="pageFont">
   <p id="HomeP">Home</p> &nbsp
+  
   <p id="Triangle1P">&#9658</p>
+  
   <p id="ContactP">Contact</p> 
+  
   <p id="Trinagle2P">&#9658</p> 
+  
   <p id="ContactP">Contact us</p>
  
     <br> <br>
