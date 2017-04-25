8032bc03ee85342f705e1ef0ce31ec85fd63eb92
LenovoZ500-pc\Nikolaj <nikolaj730@hotmail.com>
2017-04-25 10:21:16 +0200

    Fjernet HTML breadcrumbs og tilføjet baggrund

--- FlatDesign/Contact_WorldKitchen.php
+++ FlatDesign/Contact_WorldKitchen.php
@@ -1,79 +1,132 @@
 <html>
     <meta charset="utf-8">
-<link rel="stylesheet"type="text/css"href="Contact - CSS.css">
-
+<link rel="stylesheet"type="text/css"href="Contact_CSS.css">
+	   <!---------------BACKGROUND----------------->
+	
+	   <!---------------BACKGROUND----------------->
     <head>
         <title>Contact us</title>
-    </head>
-    
-<body>
-    <div id="pageFont">
-    <p id="HomeP">Home</p> &nbsp
-    <p id="Triangle1P">&#9658</p>
-    <p id="ContactP">Contact</p> 
-    <p id="Trinagle2P">&#9658</p> 
-    <p id="ContactP">Contact us</p>
- 
-    <br> <br>
-    <h1>Contact us</h1>
-    
-    
-    <div class="ContactInfo">
- 
-        <h2>Worldkitchen</h2>
-        <p> Catarina Figuerdo Bettencourt        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp       +45 12 34 56 78</p>
-        <p> 3th Floor, Aalborg University       &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp      CatarinaBettencourt@gmail.com</p>
-        <div class="locationpin"> <img src="locationpin.png" height="35" width="23"> </div>
-        <p> Denmark, Aalborg 9000</p>
 
-    </div>
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="chrome=1"><![endif]-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="format-detection" content="telephone=no"/>
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
+	<link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+	<link rel="dns-prefetch" href="http://ajax.googleapis.com/">
+	<link rel="dns-prefetch" href="http://html5shim.googlecode.com/">
+	<link rel="dns-prefetch" href="http://google-analytics.com/">
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114-retina.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144-retina.png">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700,400italic">
+	<link rel="stylesheet" type="text/css" href="styles/reset.css">
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link rel="stylesheet" type="text/css" href="styles/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="styles/blog_style.css">
+
+</head>
+
+<body> <!--style="background-image:url(phonelaptop1.jpg)"-->
+<div class="backgroundImage">
+<!--[if IE]>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
+    <style>.chromeFrameInstallDefaultStyle { width: 100%; border: 5px solid #ffa700; }</style><div id="prompt"></div>
+    <script>window.attachEvent("onload", function() {CFInstall.check({mode: "overlay", node: "prompt"});});</script>
+<![endif]-->
+
+		<!---------------MENU----------------->
+<a class="scroll-point" id="home"></a>
+
+<div id="top">
+	<nav>
+		<ul id="menu">
+			<li><a href="#home">Home</a></li>
+            <li><a href="#about">About us</a></li>
+			<li><a href="#freebies">Blog</a></li>
+			<li><a href="#contact">Contact us</a></li>
+			<li><a href="#payment">Get tickets</a></li>
+		</ul>
+	
+	<form class="menu-dropdown hidden">
+			<select onchange="location = this.options[this.selectedIndex].value;">
+				<option value="#home">Home</option>
+                <option value="#about">About us</option>
+            	<option value="#freebies">Blog</option>
+            	<option value="#contact">Contact us</option>
+            	<option value="payment">Get tickets</option>
+			</select>
+		</form>
+		<a href="#menu-footer" class="menu-btn"></a>
+	</nav>
 </div>
+		<!---------------MENU----------------->
    
- <form action="###" method="POST">
+   <section>
+    <h1>Contact us</h1>
+	 
+		<!---------------COMPANY INFORMATION----------------->    
+    <div class="ContactInfo">
+	<h2>Worldkitchen</h2><br><br><br>      
+	Catarina Figuerdo Bettencourt   <br>+45 12 34 56 78<br>
+	<!--<div class="locationpin"> <img src="locationpin.png" height="45" width="33"> </div>-->
+	3th Floor, Aalborg University  <br>CatarinaBettencourt@gmail.com<br>
+    Denmark, Aalborg 9000
+    </div>
+		<!---------------COMPANY INFORMATION----------------->
+		
+		<!---------------USERINPUT----------------->
+<form action="###" method="POST">
    
-    <div class="InformationFields">
-            
-        <input type="text" id="nameField" name="name" placeholder= "Your Name:" required="required"> <br><br>
+    <div class="InformationFields">    
+		<input type="text" id="nameField" name="name" placeholder= "Your Name:" required="required"> <br><br>
         <input type="text" id="topicField" name="topic" placeholder= "Your Topic:" required="required"> <br><br>
         <input type="text" id="mailField" name="mail" placeholder= "Your Mail:" required="required"> <br><br>
         <input type="text" id="phonenumberField" name="phonenumber" placeholder= "Your Phonenumber:" required="required"> <br><br>
         <input type="text" id="messageField" name="message" placeholder="Your Message:" required="required" 
     </div>    
 
-   <div>
+	<div>
         <input type="submit" id="submitField" value="Submit"> 
     </div>
-   
- </form>
-  <div id="pageFont">  
-   <div class="Footer1">
+	</section> 
+</form>
+		<!---------------USERINPUT----------------->
+
+		<!---------------FOOTER-------------------->
+<div id="pageFont"> 
+	<div class="Footer1">
     
     <h3>Menu &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Contact</h3>
     <!--<h4>Contact</h4> -->
-      <div class="footerBoxes">
+    <div class="footerBoxes">
         <ul type="square">
-   <li>Home</li> 
-   <li>Blog</li> 
-   <li>About</li> 
-   <li>Contact</li> 
-   <li>Buy ticket</li>
+			<li>Home</li> 
+			<li>Blog</li> 
+			<li>About</li> 
+			<li>Contact</li> 
+			<li>Buy ticket</li>
          </ul>
-      </div>
+    </div>
       
     <div class=footerContactInfo>
       Name (Catarina) <br>
       Address (Gammeltorv) <br>
       Region (Aalborg) <br>
-      Country (Denmark) <br> <br>
+      Country (Denmark) <br>
       Email@email.com <br>
       +45 69696969
     </div>
+   </div>
 
  
    <div class="Footer2">
     Copyright © 2017 Worldconflict. All rights reserved.
    </div>
-   </div> 
+</div>
+		<!---------------FOOTER----------------->
+</div>
 </body>
 <?php
 
