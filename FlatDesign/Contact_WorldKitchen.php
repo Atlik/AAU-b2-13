57e90e06e3ffe50dfab6e36504311377fa73897e
LenovoZ500-pc\Nikolaj <nikolaj730@hotmail.com>
2017-04-27 09:53:22 +0200

    New design Added FAQ Updated contact info

--- FlatDesign/Contact_WorldKitchen.php
+++ FlatDesign/Contact_WorldKitchen.php
@@ -74,13 +74,15 @@
 		<!---------------MENU----------------->
    
    <section>
-    <h1>Contact us</h1>
 	 
 		<!---------------COMPANY INFORMATION----------------->    
 	<div class="contactTitle">Worldkitchen</div>
 	<div class="ContactInfo">     
-	Catarina Figuerdo Bettencourt   <br>+45 12 34 56 78<br>
-	3th Floor, Aalborg University  <br>CatarinaBettencourt@gmail.com<br>
+	Worldkitchen's service <span class="Whitespace"></span> +45 91 94 16 13 <br><br>
+	Folkekirkens hus, gammel torv 4
+	
+	<span class="Whitespace2"></span> <img src="envelope_image.jpg" alt="Mountain View" style="width:20px;height:30px;
+	margin-top: -5px; position:absolute"> <span class="Whitespace3"></span>verdenskoekkenet@gmail.com<br><br>
     Denmark, Aalborg 9000
     </div>
 		<!---------------COMPANY INFORMATION----------------->
@@ -89,61 +91,95 @@
 
   <form action="Contact_WorldKitchen.php" method="POST"> 
     <div class="InformationFields">    
-		<input type="text" id="nameField" style="border:1px solid #ff0000" name="name" placeholder= "Your Name:" required="required"> <br><br>
-        <input type="text" id="topicField" name="topic" placeholder= "Your Topic:" required="required"> <br><br>
-        <input type="text" id="mailField" name="mail" placeholder= "Your Mail:" required="required"> <br><br>
-        <input type="text" id="phonenumberField" name="phonenumber" placeholder= "Your Phonenumber:" required="required"> <br><br>
-        <input type="text" id="messageField" name="message" placeholder="Your Message:" required="required" maxlength="10"> 
+		<input type="text" id="nameField" style="border:1px solid grey" name="name" placeholder= "Your Name:" required="required"> <br><br>
+       <!-- <input type="text" id="topicField" style="border:1px solid grey" name="topic" placeholder= "Your Topic:" required="required"> <br><br>-->
+        <input type="text" id="mailField" style="border:1px solid grey" name="mail" placeholder= "Your Mail:" required="required"> <br><br>
+        <input type="text" id="phonenumberField" style="border:1px solid grey" name="phonenumber" placeholder= "Your Phonenumber:" required="required"> <br><br>
+        <input type="text" id="messageField" style="border:1px solid grey" name="message" placeholder="Your Message:" required="required" maxlength="10"> 
     </div>    
 
+<div class="checkboxesMail">
+  <input type="checkbox" name="checkWorldKitchen" value="Kitchen"> To the Worldkitchen's service
+  <br><br>
+  <input type="checkbox" name="checkJournalist" value="Journalist"> To the journalist
+  <br><br>
+  <input type="checkbox" name="checkCEO" value="CEO"> To the CEO<br>
+</div>
 	<div>
         <input type="submit" id="submitField" value="Submit"> 
     </div>
 	</section> 
 </form>
 		<!---------------USERINPUT----------------->
-
+<section>
+<div class="FAQ">FAQ</div>
+<div class="FAQList">
+<ul type="disc">
+	<li><u>Where do I pay for the next event?</u></li>
+	At the menubar there is a text called "Buy ticket" By clicking on it you will get to a new site where
+	you have the opportunity to buy your ticket.
+	<br><br>
+	<li><u>How can I contact you?</u></li>
+	If you want to contact us or have a question you want answered, you can just click on the button
+	with the text "Click here" down below.
+	<br><br>
+	<li><u>Can I write my experience from previous events?</u></li>
+	Just down below the About section there is a section where you can see other people have shared their
+	experience. Below that there is a "click more" button where you will be sent over to a new site, where
+	you can write your experience
+	<br><br>
+	<li><u>How can I...?</u></li>
+	If you want to contact us or have a question you want answered, you can just click on the button
+	with the text "Click here" down below.	
+	<br><br>
+	<li><u>How can I...?</u></li>
+	If you want to contact us or have a question you want answered, you can just click on the button
+	with the text "Click here" down below.   
+</ul>
+</div>
+</section>
 		<!---------------FOOTER-------------------->
+<!--<section class="sectioFooter">-->
 	<div class="Footer1">
     <div class="menuFooter">Menu</div>
 	<div class="contactFooter">Contact</div>
     <div class="footerBoxes">
         <ul type="square">
-			<a href="BreadcrumbsHome.php"><li>Home</li> </a>
-			<a href="BreadcrumbsBlog.php"><li>Blog</li> </a>
-			<a href="BreadcrumbsAbout.php"><li>About</li> </a>
-			<a href="BreadcrumbsContact.php"><li>Contact</li> </a>
+			<a href="BreadcrumbsHome.php"><li>Home</li> </a> <br>
+			<a href="BreadcrumbsBlog.php"><li>Blog</li> </a> <br>
+			<a href="BreadcrumbsAbout.php"><li>About</li> </a> <br>
+			<a href="BreadcrumbsContact.php"><li>Contact</li> </a> <br>
 			<a href="BreadcrumbsTicket.php"><li>Buy ticket</li> </a>
          </ul>
     </div>
       
     <div class=footerContactInfo>
-      Name (Catarina) <br>
-      Address (Gammeltorv) <br>
-      Region (Aalborg) <br>
-      Country (Denmark) <br>
-      Email@email.com <br>
+      Name (Catarina) <br><br>
+      Address (Gammeltorv) <br><br>
+      Region (Aalborg) <br><br>
+      Country (Denmark) <br><br>
+      Email@email.com <br><br>
       +45 69696969
     </div>
-   </div>
+    </div>
 
- 
    <div class="Footer2">
     Copyright © 2017 Worldconflict. All rights reserved.
    </div>
+   <!--</section>-->
 		<!---------------FOOTER----------------->
 <!--</div>-->
 
 <?php
 $name_Field= $_POST['name'];
-$topic_Field= $_POST['topic'];
+/*$topic_Field= $_POST['topic'];*/
 $mail_Field= $_POST['mail'];
 $phone_Field= $_POST['phonenumber'];
 $message_Field= $_POST['message'];
 
 $fh = fopen("WorldKitchen Mail list.txt","w") or die("The file could not be created");
 	fwrite($fh, $name_Field) or die ("Could not write to file. Check if all textfields have been filled out");
-	fwrite($fh, $topic_Field) or die ("Could not write to file. Check if all textfields have been filled out");
+	/*fwrite($fh, $topic_Field) or die ("Could not write to file. Check if all textfields have been filled out");*/
 	fwrite($fh, $mail_Field) or die ("Could not write to file. Check if all textfields have been filled out");
 	fwrite($fh, $phone_Field) or die ("Could not write to file. Check if all textfields have been filled out");
 	fwrite($fh, $message_Field) or die ("Could not write to file. Check if all textfields have been filled out");
