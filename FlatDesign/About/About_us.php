4847b665bc8532fba6e6d54511275b9fa61ebfb3
LenovoZ500-pc\Nikolaj <nikolaj730@hotmail.com>
2017-04-29 17:14:23 +0200

    Kommentarer tilføjet CSS fra style tilføjet til contact.css

---
+++ FlatDesign/About/About_us.php
@@ -0,0 +1,102 @@
+<html>
+    <meta charset="utf-8">
+    <link rel="stylesheet"type="text/css"href="About_us.css">
+<head>
+    <title>About us</title>
+    <meta http-equiv="X-UA-Compatible" content="chrome=1"><![endif]-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="format-detection" content="telephone=no"/>
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
+	<link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+	<link rel="dns-prefetch" href="http://ajax.googleapis.com/">
+	<link rel="dns-prefetch" href="http://html5shim.googlecode.com/">
+	<link rel="dns-prefetch" href="http://google-analytics.com/">
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114-retina.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144-retina.png">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700,400italic">
+	<link rel="stylesheet" type="text/css" href="styles/reset.css">
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link rel="stylesheet" type="text/css" href="styles/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="styles/blog_style.css">
+</head>
+
+<body>
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
+    <style>.chromeFrameInstallDefaultStyle { width: 100%; border: 5px solid #ffa700; }</style><div id="prompt"></div>
+    <script>window.attachEvent("onload", function() {CFInstall.check({mode: "overlay", node: "prompt"});});</script>
+
+    <a class="scroll-point" id="home"></a>
+
+<div id="top">
+	<nav>
+		<ul id="menu">
+			<li><a href="#home">Home</a></li>
+            <li><a href="#about">About us</a></li>
+			<li><a href="#freebies">Blog</a></li>
+			<li><a href="#contact">Contact us</a></li>
+			<li><a href="#payment">Get tickets</a></li>
+		</ul>
+	
+	<form class="menu-dropdown hidden">
+		<select onchange="location = this.options[this.selectedIndex].value;">
+			<option value="#home">Home</option>
+            <option value="#about">About us</option>
+            <option value="#freebies">Blog</option>
+            <option value="#contact">Contact us</option>
+            <option value="payment">Get tickets</option>
+		</select>
+	</form>
+		<a href="#menu-footer" class="menu-btn"></a>
+	</nav>
+</div>
+
+<section id="breadCrumbsSection">
+<div id="breadCrumbs">
+	<a href="nyflad.php"> Home </a>
+	&nbsp &#x25BA &nbsp
+	<a href="nyflad.php">About</a>
+	&nbsp &#x25BA &nbsp
+	<a href="About_us.php"> About us </a>
+</div>
+
+<section>
+<div class="Story">
+    <h1>Why Worldkitchen</h1>
+    We are Anna and Catarina and this is our story.
+    <br>
+    In 2012 our paths crossed for the first time in Aalborg (Denmark) while studying
+    <br>
+    together at Aalborg University on the IMA Culture, Communication and Globalisation.
+    <br>
+    The passion for food and people was our first connecting tie, and has been so far.
+    <br>
+    While our backgrouns differ, our passions enforced a fated friendship and The World
+    <br>
+    Kitchen Partnership
+    <br><br>
+    Together we are foodies, aware consumers and eager communicators. Passionate
+    <br>
+    about social diversity and excited about the opportunities multiculturalism offers.
+    <br><br><br><br>
+    Our staff
+	<div class=staffImages>
+		<div class="CatarinaDiv">
+		<img src="Catarina_Worldkitchen.jpg" alt="CatarinaImage" style="width: 400px; height: 400px;">
+		</div>
+
+		<div class="AnnaDiv">
+		<img src="Anna_Worldkitchen.jpg" alt="CatarinaImage" style="width: 400px; height: 400px;">
+		</div>
+	</div>
+</div>
+</section>
+
+<?php
+
+?>
+    </body>
+</html>
--- FlatDesign/contact/Contact_WorldKitchen.php
+++ FlatDesign/contact/Contact_WorldKitchen.php
@@ -1,45 +1,33 @@
 <html>
     <meta charset="utf-8">
-<link rel="stylesheet"type="text/css"href="Contact_CSS.css">
-	   <!---------------BACKGROUND----------------->
-	
-	   <!---------------BACKGROUND----------------->
     <head>
         <title>Contact us</title>
-
-	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="chrome=1"><![endif]-->
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="chrome=1"><![endif]--> <!--specifies that Google Chrome frame should start if the user has it installed-->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="format-detection" content="telephone=no"/>
-	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
-	<link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+	<meta name="format-detection" content="telephone=no"/> <!--Makes sure that phonenumbers don't get flagged as links-->
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" /> 
+	<link rel="dns-prefetch" href="http://fonts.googleapis.com/"> <!--The user's visible area of a web page.-->
 	<link rel="dns-prefetch" href="http://ajax.googleapis.com/">
 	<link rel="dns-prefetch" href="http://html5shim.googlecode.com/">
 	<link rel="dns-prefetch" href="http://google-analytics.com/">
-	<link rel="shortcut icon" href="img/favicon.ico">
-	<link rel="apple-touch-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72-ipad.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114-retina.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144-retina.png">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700,400italic">
 	<link rel="stylesheet" type="text/css" href="styles/reset.css">
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
 	<link rel="stylesheet" type="text/css" href="styles/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="styles/blog_style.css">
-
+	<link rel="stylesheet"type="text/css"href="Contact_CSS.css">
 </head>
 
 <body>
-<!--<div class="backgroundImage">-->
 <!--[if IE]>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
     <style>.chromeFrameInstallDefaultStyle { width: 100%; border: 5px solid #ffa700; }</style><div id="prompt"></div>
     <script>window.attachEvent("onload", function() {CFInstall.check({mode: "overlay", node: "prompt"});});</script>
 <![endif]-->
 
-		<!---------------MENU----------------->
+		<!---------------MENU BEGIN----------------->
 <a class="scroll-point" id="home"></a>
 
-<div id="top">
+<div id="top"> <!--Displays the top menu and allows the user to click on a link and be taken to that section-->
 	<nav>
 		<ul id="menu">
 			<li><a href="#home">Home</a></li>
@@ -48,57 +36,46 @@
 			<li><a href="#contact">Contact us</a></li>
 			<li><a href="#payment">Get tickets</a></li>
 		</ul>
-	
-	<form class="menu-dropdown hidden">
-			<select onchange="location = this.options[this.selectedIndex].value;">
-				<option value="#home">Home</option>
-                <option value="#about">About us</option>
-            	<option value="#freebies">Blog</option>
-            	<option value="#contact">Contact us</option>
-            	<option value="payment">Get tickets</option>
-			</select>
-		</form>
-		<a href="#menu-footer" class="menu-btn"></a>
 	</nav>
 </div>
 
-<section id="breadCrumbsSection">
+
+<section id="breadCrumbsSection"> <!--Displays which section you are on, on the website. Eg: Home ► Contact ► Contact us -->
 	<div id="breadCrumbs">
-			<a href="nyflad.php"> Home </a>
+			<a href="nyflad.php"> Home </a> <!--Allows users to click the link and be taken to that section on the website-->
 			&nbsp &#x25BA &nbsp
 			<a href="nyflad.php">Contact</a>
 			&nbsp &#x25BA &nbsp
 			<a href="Contact_WorldKitchen.php"> Contact us </a>
 	</div>
 </section>
-		<!---------------MENU----------------->
+		<!---------------MENU END----------------->
    
-   <section>
+<section>
 	 
-		<!---------------COMPANY INFORMATION----------------->    
-	<div class="contactTitle">Worldkitchen</div>
+		<!---------------COMPANY INFORMATION BEGIN----------------->    
+	<div class="contactTitle">Worldkitchen</div> <!--Displays the information of the company-->
 	<div class="ContactInfo">     
 	Worldkitchen's service <span class="Whitespace"></span> +45 91 94 16 13 <br><br>
 	Folkekirkens hus, gammel torv 4
 	
-	<span class="Whitespace2"></span> <img src="envelope_image.jpg" alt="Mountain View" style="width:20px;height:30px;
+	<span class="Whitespace2"></span> <img src="envelope_image.jpg" alt="EnvelopePicture" style="width:20px;height:30px;
 	margin-top: -5px; position:absolute"> <span class="Whitespace3"></span>verdenskoekkenet@gmail.com<br><br>
     Denmark, Aalborg 9000
     </div>
-		<!---------------COMPANY INFORMATION----------------->
+		<!---------------COMPANY INFORMATION END----------------->
 		
-		<!---------------USERINPUT----------------->
+		<!---------------USERINPUT BEGIN----------------->
 
-  <form action="Contact_WorldKitchen.php" method="POST"> 
+  <form action="Contact_WorldKitchen.php" method="POST"> <!--Allows users to input name, mail, phonenumber, and a message and submit it-->
     <div class="InformationFields">    
 		<input type="text" id="nameField" style="border:1px solid grey" name="name" placeholder= "Your Name:" required="required"> <br><br>
-       <!-- <input type="text" id="topicField" style="border:1px solid grey" name="topic" placeholder= "Your Topic:" required="required"> <br><br>-->
         <input type="text" id="mailField" style="border:1px solid grey" name="mail" placeholder= "Your Mail:" required="required"> <br><br>
         <input type="text" id="phonenumberField" style="border:1px solid grey" name="phonenumber" placeholder= "Your Phonenumber:" required="required"> <br><br>
         <input type="text" id="messageField" style="border:1px solid grey" name="message" placeholder="Your Message:" required="required" maxlength="10"> 
     </div>    
 
-<div class="checkboxesMail">
+<div class="checkboxesMail"> <!--Allows user choose to whome the email should be sent to-->
   <input type="checkbox" name="checkWorldKitchen" value="Kitchen"> To the Worldkitchen's service
   <br><br>
   <input type="checkbox" name="checkJournalist" value="Journalist"> To the journalist
@@ -108,11 +85,11 @@
 	<div>
         <input type="submit" id="submitField" value="Submit"> 
     </div>
-	</section> 
-</form>
-		<!---------------USERINPUT----------------->
+	</form>
+</section> 
+		<!---------------USERINPUT END----------------->
 <section>
-<div class="FAQ">FAQ</div>
+<div class="FAQ">FAQ</div> <!--Displays the Frequently asked questions-->
 <div class="FAQList">
 <ul type="disc">
 	<li><u>Where do I pay for the next event?</u></li>
@@ -138,14 +115,13 @@
 </ul>
 </div>
 </section>
-		<!---------------FOOTER-------------------->
-<!--<section class="sectioFooter">-->
-	<div class="Footer1">
+		<!---------------FOOTER BEGIN-------------------->
+	<div class="Footer1"> <!--Displays the footer of the website-->
     <div class="menuFooter">Menu</div>
 	<div class="contactFooter">Contact</div>
     <div class="footerBoxes">
         <ul type="square">
-			<a href="BreadcrumbsHome.php"><li>Home</li> </a> <br>
+			<a href="BreadcrumbsHome.php"><li>Home</li> </a> <br> <!--Allows the users to click an anchor element and they will be taken to that section-->
 			<a href="BreadcrumbsBlog.php"><li>Blog</li> </a> <br>
 			<a href="BreadcrumbsAbout.php"><li>About</li> </a> <br>
 			<a href="BreadcrumbsContact.php"><li>Contact</li> </a> <br>
@@ -154,36 +130,36 @@
     </div>
       
     <div class=footerContactInfo>
-      Name (Catarina) <br><br>
+      Name (Worldkitchen) <br><br>
       Address (Gammeltorv) <br><br>
       Region (Aalborg) <br><br>
       Country (Denmark) <br><br>
-      Email@email.com <br><br>
+      verdenskoekkenet@gmail.com <br><br>
       +45 69696969
     </div>
     </div>
 
-   <div class="Footer2">
-    Copyright © 2017 Worldconflict. All rights reserved.
+   <div class="Footer2"> <!--displayed at the bottom of the page. For copyright claims-->
+    Copyright © 2017 Worldkitchen. All rights reserved.
    </div>
    <!--</section>-->
-		<!---------------FOOTER----------------->
+		<!---------------FOOTER END----------------->
 <!--</div>-->
 
 <?php
-$name_Field= $_POST['name'];
-/*$topic_Field= $_POST['topic'];*/
+//Supposed to work with the "User input" section. Which will send an email to The world kitchen. It will send a notepad file.
+$name_Field= $_POST['name']; //Variable names that will store the values of whatever the user inputs in the textfields on the website
 $mail_Field= $_POST['mail'];
 $phone_Field= $_POST['phonenumber'];
 $message_Field= $_POST['message'];
 
-$fh = fopen("WorldKitchen Mail list.txt","w") or die("The file could not be created");
-	fwrite($fh, $name_Field) or die ("Could not write to file. Check if all textfields have been filled out");
+$fh = fopen("WorldKitchen Mail list.txt","w") or die("The file could not be created"); //Opens a notepad file
+	fwrite($fh, $name_Field) or die ("Could not write to file. Check if all textfields have been filled out"); //Writes the info to the file
 	/*fwrite($fh, $topic_Field) or die ("Could not write to file. Check if all textfields have been filled out");*/
 	fwrite($fh, $mail_Field) or die ("Could not write to file. Check if all textfields have been filled out");
 	fwrite($fh, $phone_Field) or die ("Could not write to file. Check if all textfields have been filled out");
 	fwrite($fh, $message_Field) or die ("Could not write to file. Check if all textfields have been filled out");
-fclose($fh);
+fclose($fh); //Closes file.
 ?>
 </body>
 </html>
